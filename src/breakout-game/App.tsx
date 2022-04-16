b9d3b159bf0134500dd6b1128ecad5c8d218639f
Master-Hash <A137294381b@163.com>
2022-04-16 23:52:12 +0800

    feat(breakout-game): :construction: 完成教程第二节

--- src/breakout-game/App.tsx
+++ src/breakout-game/App.tsx
@@ -7,24 +7,26 @@ export function BreakoutGame() {
     // console.log(canvas);
 
     const ctx = canvas.getContext("2d")!;
+    // 疑惑：`let` 怪怪的
+    let x = canvas.width / 2;
+    let y = canvas.height - 30;
+    const dx = 2;
+    const dy = -2;
 
-    ctx.beginPath();
-    ctx.rect(20, 40, 50, 50);
-    ctx.fillStyle = "#FF0000";
-    ctx.fill();
-    ctx.closePath();
-
-    ctx.beginPath();
-    ctx.arc(240, 160, 20, 0, Math.PI * 2, false);
-    ctx.fillStyle = "green";
-    ctx.fill();
-    ctx.closePath();
-
-    ctx.beginPath();
-    ctx.rect(160, 10, 100, 40);
-    ctx.strokeStyle = "rgba(0, 0, 255, 0.5)";
-    ctx.stroke();
-    ctx.closePath();
+    function draw() {
+      ctx.clearRect(0, 0, canvas.width, canvas.height);
+      ctx.beginPath();
+      ctx.arc(x, y, 10, 0, Math.PI * 2);
+      ctx.fillStyle = "#0095DD";
+      ctx.fill();
+      ctx.closePath();
+      x += dx;
+      y += dy;
+    }
+    const drawID = setInterval(draw, 10);
+    return () => {
+      clearInterval(drawID);
+    };
   }, []);
 
   return <>
